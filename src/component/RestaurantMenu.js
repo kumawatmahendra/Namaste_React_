8518fd25b110ab1cb77b1835f5c91da76b1dfb07
kumawatmahendra <kumawatmahendra7896@gmail.com>
2023-08-30 21:23:35 +0530

    commit 3

--- src/component/RestaurantMenu.js
+++ src/component/RestaurantMenu.js
@@ -1,26 +1,45 @@
-import {useEffect} from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/contants";
 
-const RestaurantMenu = ()=>{
-     
-    useEffect(()=>{
+const RestaurantMenu = () => {
+
+    const [resInfo, setResInfo] = useState(null)
+    const { resId } = useParams()
+
+    useEffect(() => {
         fetchMenu()
-    },[])
-    
-    const fetchMenu = async () =>{
-       const data = await fetch()
-       const result = data.json("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7272832&lng=77.3370089&restaurantId=32128&catalog_qa=undefined&submitAction=ENTER%22")
-       
+    }, [])
+    //MENU_API+resId
+    const fetchMenu = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7272832&lng=77.3370089&restaurantId="+resId+"&catalog_qa=undefined%22")
+        const json = await data.json()
+        console.log(json.data);
+        setResInfo(json.data)
     }
-    // json_data?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    return(
+
+
+    const { itemCards } =resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.info || []
+    console.log(itemCards);
+
+    const{ name, cuisines, avgRating, costForTwoMessage }=
+     resInfo?.cards[0]?.card?.card?.info || []
+
+    if (resInfo === null) return <Shimmer />
+
+    return (
         <div className="menu">
-            <h1>Name of the Restaurant</h1>
-            <h2>Menu</h2>
+            <h1>{name}</h1>
+            <p>{cuisines} - {costForTwoMessage}</p>
+            <h3></h3>
+            <h3>{avgRating}</h3>
+            <h2>menu</h2>
             <ul>
-                <li>briyani</li>
-                <li>burger</li>
-                <li>coldrink's</li>
-            </ul>
+                {itemCards.map((items) => (
+                    <li key={items.card.info.id}>{items.card.info.name} - Rs.{items.card.info.price/100}</li>
+                ))}
+        </ul>
         </div>
     )
 }
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,3 +1,5 @@
 export const LOGO_URL= "https://png.pngtree.com/template/20210107/ourmid/pngtree-eco-fresh-food-logo-image_471909.jpg"
 
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7272832&lng=77.3370089&restaurantId="
